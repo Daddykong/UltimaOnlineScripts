e95805e1eb473200350ae92bd5cade97f3c9500a
Daddykong <75580310+Daddykong@users.noreply.github.com>
2025-10-16 10:43:15 -0400

    Create Auto_Heal.js

---
+++ Auto_Heal.js
@@ -0,0 +1,31 @@
+//TODO: Add poisoning check and fix, maybe curse check and fix?
+
+while (true) {
+  const bandageType = 0xe21;
+  const bandages = client.findType(bandageType);
+
+  if (player.hits <= player.maxHits * .8) {
+    if (player.getSkill(Skills.Healing).value > 50 && bandages) {
+      player.use(bandages);
+      target.waitTargetSelf();
+      //client.headMsg('Bandage Heal', player.serial);
+      //It takes 6 seconds to apply bandages so sleep for 4 at least
+      sleep(4000)
+    }
+  }
+  if (player.hits <= player.maxHits * .6) {
+    if (player.getSkill(Skills.Magery).value > 50) {
+      player.cast(Spells.GreaterHeal);
+      target.waitTargetSelf();
+      //client.headMsg('Greater Heal', player.serial);
+      sleep(3000)
+    }
+    if (player.getSkill(Skills.Chivalry).value > 50) {
+      player.cast(Spells.CloseWounds);
+      target.waitTargetSelf();
+      //client.headMsg('Close Wounds', player.serial);
+      sleep(3000)
+    }
+  }
+  sleep(500);
+}
